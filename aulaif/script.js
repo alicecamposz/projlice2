aa1c8ffd326c38d4c3351e37420c98975cb61e1b
licegithub <camposalicesouza@gmail.com>
2026-05-05 17:03:14 -0300

    Add files via upload

---
+++ aulaif/script.js
@@ -0,0 +1,166 @@
+alert ("10")
+alert ("Alice Campos")
+
+function ceu() { 
+    let numero = document.getElementById ("nuvem")
+    if (numero = 10){
+        alert("Correto!!")
+    }
+}
+
+function ceu1() {
+    let num = document.getElementById ("nuvem1")
+    if (num = 5){
+        alert("Correto!")
+    }
+}
+
+function ceu2() {
+    let texto = document.getElementById ("nuvem2");
+    if (texto = "admin") {
+        frank.textContent = "Correto!!"
+    }
+}
+  
+function ceu3() {
+    let texto1 = document.getElementById ("mudar");
+    if (texto1 = "teste") {
+    mudar.textContent = "alicecampos";
+ }
+}
+  
+function quinto(){
+
+    let cor = document.getElementById("num5").value;
+    if (cor =="Azul")
+    {
+            document.getElementById("soso").style.backgroundColor = "blue";
+    }
+}
+
+function sexto(){
+
+    let maior = document.getElementById("num6").value;
+    if (maior >=10) 
+    {
+        alert("Correto");
+    }
+}
+function setimo(){
+
+    let maior3 = document.getElementById("num7").value;
+    if (maior3 <=50) 
+    {
+        alert("Correto");
+    }
+}
+
+function oitavo(){
+
+    let mudar2 = document.getElementById("num8").value;
+    if (mudar2 =="senha")
+    {
+            document.getElementById("gabi").textContent = "e linda tambem";
+    }
+}
+
+function nono(){
+
+    let cor2 = document.getElementById("num9").value;
+    if (cor2 =="escuro")
+    {
+            body1.style.backgroundColor = "grey";
+    }
+}
+
+function decimo(){
+
+    let mais = document.getElementById("num10").value;
+    if (mais == "alterar")
+    {
+            lulu.textContent = "Mudou!";
+            lulu.style.backgroundColor = "Red";
+    }
+}
+
+function decimoprime(){
+
+    let tchu = document.getElementById("num11").value;
+    if (tchu == "ok")
+    {
+            lice1.textContent = "licelinda"
+            lice1.style.fontSize = "30px";
+            lice1.style.color = "Red";
+    }
+}
+
+function decimoseg(){
+
+    let fonte = document.getElementById("num12").value;
+    if (fonte >=20) 
+    {
+         document.getElementById("alice").style.fontSize = "30px";
+    }
+}
+
+function decimoter(){
+
+    let fds = document.getElementById("num13").value;
+    if (fds =="admin")
+    {
+            alert("Bem-Vindo")
+            document.getElementById("penelope").textContent = "Bem-vindo";
+    }
+}
+
+function decimoquar(){
+
+    let tchu2 = document.getElementById("num14").value;
+    let maria = document.getElementById("maria");
+    if (tchu2 == 27) {
+            maria.style.fontSize = "40px";
+            maria.textContent = "Sertanejo e legal";
+            maria.style.color = "blue";
+    }
+}
+
+function decimoquin(){
+
+    let foudasse = document.getElementById("num15").value;
+    if (foudasse > 100)
+    {
+            document.getElementById("body1").style.fontSize = "40px";
+            document.getElementById("body1").style.backgroundColor = "pink";
+            document.getElementById("body1").style.color = "blue";
+    }
+}
+
+function decimosex(){
+
+    let cor = document.getElementById("num16").value;
+    if (cor >=100)
+    {
+            document.getElementById("tudo").style.backgroundColor = "purple";
+    }
+}
+
+function decimoset(){
+
+    let paiva = document.getElementById("num16").value;
+    if (paiva =="copiar")
+    {
+            document.getElementById("paivaa").textContent = "Copiar!";
+    }
+}
+
+function decimooit(){
+
+    let malev = document.getElementById("num17").value;
+    if (malev ==5)
+    {
+            document.getElementById("malevola").style.backgroundColor = "blue";
+            document.getElementById("malevola2").style.backgroundColor = "blue";
+    }
+}
+
+
